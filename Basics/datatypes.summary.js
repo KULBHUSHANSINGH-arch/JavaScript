ff5e068a9d54309904b900358ad15cb759ed6319
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-01 13:03:02 +0000

    datatype summary

---
+++ Basics/datatypes.summary.js
@@ -0,0 +1,59 @@
+// Primit Data Types 
+
+/*
+String 
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+*/
+
+const score = 100
+const scoreValue  = 101.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('12344')
+const anotherId = Symbol('12344')
+
+console.log(id === anotherId);
+
+// const bigNumber = 45526588627584514514551452n;
+
+
+
+// Non Premitive ( Reference Data Type) 
+/*
+Array
+Objects
+Functions
+*/
+
+
+// *********** Array Decleration ***********
+const heros =  ["shaktiman", "Doremon", "JuniorG"];
+
+// *********************** Objects ***********************
+
+let myObje = {
+    name: "saurabh",
+    age: 23,
+    city: "****",
+    State: "jsja"
+}
+
+// ************** Function *******************
+const myFunction = function() {
+    console.log("hello world");
+}
+
+// ************** how to checkd the data type*****************
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+
+console.log(typeof myFunction);
