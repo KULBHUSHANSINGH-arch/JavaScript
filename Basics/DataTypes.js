2879c8e5461afef0370991de2dbce3d04c56ef09
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-01 09:03:04 +0000

    datatype

---
+++ Basics/DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3);
+
+let name = "kulbhushan singh"       // string type
+let age = 45    ;            // numbers 
+let isLoggedIn =  true/ false  ;     // Boolean type
+
+// Number  ---> 2  to power 52/53
+// Bigint   ------> larger numbers to be used ( like a Trading )
+// sring =----------->     " "
+// boolean --------------> True/ False 
+
+
+// null --------> standelone Value   (null is representation of Empty Vlaue )  (it's Output will be a "object")
+// Undefined ------------> always defined the Undefined value 
+// Symbol ----------> unique ness 
+
+
+// ******************* Objects **************
+console.log(typeof "kulbhushan Singh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
