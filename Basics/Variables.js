206a0e77b97f0b536c224c060a44debdde670b03
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-01 13:04:16 +0000

    variables

---
+++ Basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 143278
+let accountEmail = "example121@gamil.com"
+var accountPassword = "143290"
+accountCity = " jaipur"
+let accountState ;
+// accountId = 23
+accountEmail = "devloper@MediaList.com"
+accountPassword= "616926"
+accountCity= "Delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword,accountCity, accountState])
