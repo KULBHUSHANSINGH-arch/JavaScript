eebd9701b64cc6cbbee67dbb6b47dc8d7f776743
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-01 09:47:37 +0000

    conversion of Data Types

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,87 @@
+
+// let score = "33abcd";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ***************** check the Null data type **************
+
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ********************************** check the Undefined data type ****************
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ********************************** check the Undefined data type ****************
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//  ****************************** check it Name **************
+// let score = KUlbhushan
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/* 
+ "3333" ---------->  333 
+ "33abc" --------> NaN
+ true ---------> 1 
+ false -------> 0
+*/
+
+
+
+// ************ let me checked the Boolean function will be changed or Not ********************************
+/*
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+*/
+// *******check empty string (conversation)
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+/* NOTES
+1 ---> true or   0 ------> false
+"  " ------> false
+"name  " -----> true
+*/
+
+
+
+// ******************** Conversation with String ********************
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
