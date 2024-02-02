ba730d3573dc71a08c7d98bceaa8d863a8f8260e
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-02 10:01:25 +0000

    date

---
+++ Basics/date_and_time.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+console.log(myDate)
+/*
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
+console.log(myDate.toUTCString())
+console.log(myDate.getTimezoneOffset())
+console.log(myDate.getDate())
+console.log("month",myDate.getMonth())
+*/
+
+let myCreatedDate = new Date(2024,0, 2)
+
+console.log(myCreatedDate.toDateString())
