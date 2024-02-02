b7063bcccf5b28b9b6c0336183348a8637ad110b
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-02-02 09:17:37 +0000

    maths and nums

---
+++ Basics/nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400;
+// console.log(score)
+
+const balance = new Number(100)
+/*
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(1))
+*/
+
+const otherNumber = 123.8967
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds  = 100000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ######################################----------------------------- Maths ------------------------------####
+
+console.log(Math)
+/*
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.8))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.3))
+
+console.log(Math.min(23,2,13,0.2,1000,223,43,2))
+console.log(Math.max(23,2,13,0.2,1000,223,43,2))
+
+*/
+
+// console.log(Math.random())
+// console.log(Math.random()*10 + 1)
+// console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min +1 )) + min)
+
